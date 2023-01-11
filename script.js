170170b3446d4a0c1454bf6c79203de8f425443c
Yesid Vanegas <yesidvanegas68@gmail.com>
2023-01-10 19:19:42 -0500

    Entrega de ejercicios Tarea 2 comandos basicos

---
+++ script.js
@@ -0,0 +1,18 @@
+/*Realizar un algoritmo en JavaScript que permita calcular la suma, resta, multiplicación y división de dos números,
+ donde num1 = 5 y num2 = 7. Mostrar los resultados por consola.*/
+
+ let num1 = 5;
+ let num2 = 7;
+ let sum=0,res=0,multi=0,div=0;
+
+ sum = num1+num2;
+ res = num1-num2;
+ multi = num1*num2;
+ div = num1/num2;
+
+ alert (`Operaciones basicas con los numeros ${num1} y ${num2}: \n 
+ Suma: ${sum} \n 
+ Resta: ${res} \n 
+ Multipliacion: ${multi} \n 
+ Division: ${div}`);
+
